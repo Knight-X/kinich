15bdfdaff1b16107794079c0738792c37d18735d
kazami <kazamizero@gmail.com>
2017-02-18 13:07:12 +0800

    for loading the dataset

--- gradient.hpp
+++ gradient.hpp
@@ -1,14 +1,14 @@
 #pragma once
 #include "nn_datatype.hpp"
+#include "lossfunc.hpp"
 
 namespace nn {
-  template<typename LossFunc>
-  nn_vec_t gradient(const nn_vec_t* y, const nn_vec_t* t)
+  nn_vec_t gradient(mean_square_root* func, const nn_vec_t* y, const nn_vec_t* t)
   {
     nn_vec_t grad(y->size());
     
     for (nn_size s = 0; s < y->size(); s++) {
-	grad[s] = LossFunc::derivative_func(y[s], t[s]);
+	grad[s] = func->derivative_func(y[s], t[s]);
     }
     return grad;
   }
