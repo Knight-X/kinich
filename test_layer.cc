e90ff9006148e8e32d8f0616108f46c73cd088f9
kazami <kazamizero@gmail.com>
2017-02-18 13:04:52 +0800

    add the test case for layer

--- test_layer.cc
+++ test_layer.cc
@@ -2,6 +2,7 @@
 #include "layer.hpp"
 #include "input.hpp"
 #include "fully_connect.hpp"
+#include "io.cpp"
 
 
 using ::testing::TestWithParam;
@@ -34,13 +35,27 @@ class LayerTest : public QuickTest {
 };
 
 TEST_F(LayerTest, DefaultTest) {
-  nn::nn_vec_t in = nn::nn_vec_t(10, 0); 
-  nn::nn_vec_t gy = nn::nn_vec_t(10, 2);
-  nn::nn_vec_t ans = nn::nn_vec_t(10, 0.5);
+  nn::nn_vec_t in; 
+  nn::nn_vec_t gy;
+  loadFile("letter-recognition-2.csv", 16, 3, &in, &gy);
+  nn::nn_vec_t ans = nn::nn_vec_t(10, 0.999977);
   EXPECT_EQ(0, l1.input_dim());
   EXPECT_EQ(10, l2.input_dim());
-  EXPECT_EQ(in, l1.forward_prop(in, 0));
-  EXPECT_EQ(in, l1.backward_prop(in, 0));
-  EXPECT_EQ(ans, l2.forward_prop(in, 0));
+  const nn::nn_vec_t* tmp = l1.forward_prop(&in, 0);
+  nn::nn_vec_t res = *tmp;
+  for (int i = 0; i < res.size(); i++) {
+    EXPECT_EQ(in[i], res[i]);
+  }
+  EXPECT_EQ(l1.result(), l1.forward_prop(&in, 0));
+  EXPECT_EQ(&in, l1.backward_prop(&in, 0));
+  //EXPECT_EQ(l2.result(), l2.forward_prop(&ans, 0));
+  const nn::nn_vec_t* tmp2 = l2.forward_prop(&in, 0);
+  nn::nn_vec_t res2 = *tmp2;
+  for (int i = 0; i < res2.size(); i++) {
+	//std::cout << in[i] << " ";
+  	EXPECT_EQ(ans[i], res2[i]);
+	//std::cout << res2[i] << " ";
+  } 
+
 }
       
