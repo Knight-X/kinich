fec86d3563198989692c8c7211794e1eee0f0c25
kazami <kazamizero@gmail.com>
2017-02-23 14:08:28 +0000

    for fprop test initially success

--- test_nn.cc
+++ test_nn.cc
@@ -6,6 +6,7 @@
 #include "optimizer.hpp"
 #include "lossfunc.hpp"
 #include "graph.hpp"
+#include "io.cpp"
 
 
 using ::testing::TestWithParam;
@@ -41,11 +42,40 @@ protected:
     nn::Graph *g = new nn::Graph();
     nn::mean_square_root* r = new nn::mean_square_root();
     nn::Optimizer* o = new nn::stochastic_gradient_descent();
+    nn::input_layer l1 = nn::input_layer();
+    nn::fully_connected_layer<nn::activation::sigmoid> l2 = nn::fully_connected_layer<nn::activation::sigmoid>(10, 10);
     nn::NNetwork nnet {g, o, r};
 };
 
 TEST_F(NNnetTest, DefaultTest)
 {
+    nn::nn_vec_t in;
+    nn::nn_vec_t gy;
+    loadFile("letter-recognition-2.csv", 16, 3, &in, &gy);
     EXPECT_EQ(g, nnet.getGraph());
+    nn::nn_vec_t ans = nn::nn_vec_t(10, 0);
+    nn::nn_vec_t out = nn::nn_vec_t(10, 0);
+    nn::nn_vec_t& weight = l2.weight();
+    nn::nn_vec_t& bias = l2.bias();
+    nn::nn_size out_dim = l2.output_dim();
+    nn::nn_size in_dim = l2.input_dim();
+    nn::activation::activation_interface& h_ = l2.activation_func();
+    for (nn::nn_size i = 0; i < out_dim; i++) {
+        for (nn::nn_size c = 0; c < in_dim; c++) {
+            ans[i] += weight[c * out_dim + i] * in[c];
+        }
+        ans[i] += bias[i];
+    }
+    for (nn::nn_size i = 0; i < out_dim; i++) {
+        out[i] = h_.result(ans, 0);
+    }
+    nnet.add(&l1);
+    nnet.add(&l2);
+    const nn::nn_vec_t* tmp2 = nnet.fprop(in);
+    nn::nn_vec_t res = *tmp2;
+
+    for (nn::nn_size i = 0; i < res.size(); i++) 
+        EXPECT_EQ(res[i], out[i]);
+
 }
 
