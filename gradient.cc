41e6dc935c8d92982f391729b685ec145090b0e1
kazami <kazamizero@gmail.com>
2017-03-01 15:44:46 +0000

    try to make the function definition seperately, because of the multiple function definition

---
+++ gradient.cc
@@ -0,0 +1,16 @@
+#include "gradient.hpp"
+
+namespace nn
+{
+nn_vec_t gradient(mean_square_root* func, const nn_vec_t* y, const nn_vec_t* t)
+{
+    nn_vec_t grad(y->size());
+
+    const nn_vec_t& _y = *y;
+    const nn_vec_t& _t = *t;
+    for (nn_size s = 0; s < _y.size(); s++) {
+        grad[s] = func->derivative_func(_y[s], _t[s]);
+    }
+    return grad;
+}
+}
--- gradient.hpp
+++ gradient.hpp
@@ -4,15 +4,5 @@
 
 namespace nn
 {
-nn_vec_t gradient(mean_square_root* func, const nn_vec_t* y, const nn_vec_t* t)
-{
-    nn_vec_t grad(y->size());
-
-    const nn_vec_t& _y = *y;
-    const nn_vec_t& _t = *t;
-    for (nn_size s = 0; s < _y.size(); s++) {
-        grad[s] = func->derivative_func(_y[s], _t[s]);
-    }
-    return grad;
-}
+nn_vec_t gradient(mean_square_root* func, const nn_vec_t* y, const nn_vec_t* t);
 }
