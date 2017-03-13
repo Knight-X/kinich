b59872a4244a181095da1bc4c15ee77ee4189530
kazami <kazamizero@gmail.com>
2017-03-13 04:04:10 +0000

    title: add the propogation test content: complete the backward propogation test for the nn network

--- test_nn.cc
+++ test_nn.cc
@@ -45,6 +45,7 @@ protected:
     nn::Optimizer* o = new nn::stochastic_gradient_descent();
     nn::input_layer l1 = nn::input_layer();
     nn::fully_connected_layer<nn::activation::sigmoid> l2 = nn::fully_connected_layer<nn::activation::sigmoid>(16, 3);
+    std::vector<nn::nn_vec_t> resd;
     nn::NNetwork nnet {g, o, r};
 };
 
@@ -54,35 +55,44 @@ TEST_F(NNnetTest, DefaultTest)
     std::vector<std::vector<nn::nn_vec_t>> gy;
     loadFile("letter-recognition-2.csv", 16, 3, &in, &gy);
     EXPECT_EQ(g, nnet.getGraph());
-    nn::nn_vec_t ans = nn::nn_vec_t(16, 0.0);
-    nn::nn_vec_t out = nn::nn_vec_t(16, 0.0);
+    nnet.add(&l1);
+    nnet.add(&l2);
+    
+    std::vector<nn::nn_vec_t> res_fprop = nnet.fprop(in[0]);
+    
+    
+    std::cout << "fully_connected_layer test start" << std::endl;
+    //perform forward propagation
+    //const nn::nn_vec_t* tmp2 = l2.forward_prop(newin, 0);
+    const nn::nn_vec_t& res = l2.output(0);
+
+    //do forward propagation for test case 
+    nn::nn_vec_t tmp_res = nn::nn_vec_t(16, 0.0);
+    nn::nn_vec_t target_out = nn::nn_vec_t(16, 0.0);
     nn::nn_vec_t& weight = l2.weight();
     nn::nn_vec_t& bias = l2.bias();
     nn::nn_size out_dim = l2.output_dim();
     nn::nn_size in_dim = l2.input_dim();
     nn::activation::activation_interface& h_ = l2.activation_func();
-    std::cout << "fully_connected_layer test start" << std::endl;
     for (nn::nn_size i = 0; i < out_dim; i++) {
         for (nn::nn_size c = 0; c < in_dim; c++) {
-            ans[i] += weight[c * out_dim + i] * in[0][0][c];
-
+            tmp_res[i] += weight[c * out_dim + i] * in[0][0][c];
+            cout << "target in " << in[0][0][c] << endl;
         }
 
-        ans[i] += bias[i];
+        tmp_res[i] += bias[i];
     }
     for (nn::nn_size i = 0; i < out_dim; i++) {
-        out[i] = h_.result(ans, 0);
+        target_out[i] = h_.result(tmp_res, 0);
     }
-    nnet.add(&l1);
-    nnet.add(&l2);
-    const nn::nn_vec_t* newin = l1.forward_prop(&in[0][0], 0);
-    const nn::nn_vec_t* tmp2 = l2.forward_prop(newin, 0);
-    const nn::nn_vec_t& res = *tmp2;
 
-    for (nn::nn_size i = 0; i < res.size(); i++)
-        EXPECT_EQ(res[i], out[i]);
+    for (nn::nn_size i = 0; i < res.size(); i++) {
+        EXPECT_EQ(res[i], target_out[i]);
+        cout << "res" << res[i] << " ";
+        cout << "target_out" << target_out[i] << " ";
+    }
+    cout << endl;
 
-    std::vector<nn::nn_vec_t> resd;
     resd.push_back(res);
     nn::nn_vec_t delta(resd[0].size());
     nn::mean_square_root* _lossfunc = new nn::mean_square_root();
@@ -97,26 +107,50 @@ TEST_F(NNnetTest, DefaultTest)
         }
         cout << endl;
     }
-    for (nn::nn_size i = 0; i < delta.size(); i++)
+    cout << endl;
+    
+    //backward propagation
+    nn::layer_local_storage& storage = l2.get_local_storage(0);
+    const nn::nn_vec_t& prev_out = l2.prev()->output(0);
+    const nn::activation::activation_interface& prev_h = l2.prev()->activation_func();
+    nn::nn_vec_t& prev_delta = storage._layer_prev_delta;
+    nn::nn_vec_t& delta_w = storage._delta_w;
+    nn::nn_vec_t& delta_b = storage._delta_b;
+
+
+    for (nn::nn_size j = 0; j < in_dim; j++) {
+        for (nn::nn_size c = 0; c < out_dim; c++) {
+            delta_w[j * out_dim + c] += delta[c] * prev_out[j]; 
+        }
+        prev_delta[j] *= prev_h.differential_result(prev_out[j]);
+    }
+    for (nn::nn_size j = 0; j < in_dim; j++) {
+        for (nn::nn_size c = 0; c < out_dim; c++) {
+            delta_w[j * out_dim + c] += delta[c] * prev_out[j];
+        }
+    }
+
+    
+    std::vector<nn::nn_vec_t> res_bprop = nnet.bprop(res_fprop, gy[0]);
+    nn::nn_vec_t& dw = l2.weight_diff(0);
+    nn::nn_vec_t& db = l2.bias_diff(0);
+    nn::nn_vec_t d = nnet.output();
+    for (nn::nn_size i = 0; i < delta.size(); i++) {
         cout << "delta " << delta[i] << " ";
-
-    cout << endl;
-    const nn::nn_vec_t* input = &delta;
-    input  = l2.backward_prop(input, 0);
-    const nn::nn_vec_t& res_prev = *input;
-    nn::nn_vec_t& delta_w = l2.weight_diff(0);
-    nn::nn_vec_t& delta_b = l2.bias_diff(0);
-
-    for (nn::nn_size i = 0; i < delta_b.size(); i++) {
-        cout << "b" << delta_b[i] << " ";
+        EXPECT_EQ(d[i], delta[i]);
     }
-    cout << endl;
+
+   for (nn::nn_size i = 0; i < out_dim; i++) 
+            delta_b[i] += delta[i];
+
     for (nn::nn_size i = 0; i < delta_w.size(); i++) {
-        cout << "w" << delta_w[i] << " ";
+        EXPECT_EQ(delta_w[i], dw[i]);
     }
     cout << endl;
-    for (nn::nn_size i = 0; i < res_prev.size(); i++)
-        cout << "prev_res" << res_prev[i] << " ";
+    for (nn::nn_size i = 0; i < delta_b.size(); i++) {
+        EXPECT_EQ(delta_b[i], db[i]);
+    }
+    cout << endl;
+    
 
 }
-
